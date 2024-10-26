791291a158d1a6bc22b214e4de2d7bcdaad05a77
Sivasakthiperumal <sivaguna2005@gmail.com>
2024-10-27 00:16:46 +0530

    Add files via upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import End from './Components/End/End'
+import Header from './Components/Header/Header'
+
+import Home from './Components/Home/Home'
+
+function App() {
+  
+  return (
+    <>
+     <Header/>
+     <Home/>
+    <End/>
+     
+
+    </>
+  )
+}
+
+export default App
